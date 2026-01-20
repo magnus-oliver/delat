c5f2d9b5796df1981e707bcd5e9e5f6f60c29760
magnus-oliver <olivermagnusson05@gmail.com>
2026-01-20 14:51:05 +0100

    Uppdaterade Z2-3

--- Z2-3.py
+++ Z2-3.py
@@ -1,7 +1,7 @@
-hpVoltage = 5.6237
+hpVoltage = 5.6237 # ersätt med ert mätvärde
 deltaHP = hpVoltage*0.0035/100+0.0005*8/100
 print("Mätosäkerhet spänning (mV):", deltaHP*1000)
 
-agilentCurrent = 0.239
+agilentCurrent = 0.239 # ersätt med ert mätvärde
 deltaAgilent = agilentCurrent*0.02/100+0.03
 print("Mätosäkerhet ström (mA):", deltaAgilent)
